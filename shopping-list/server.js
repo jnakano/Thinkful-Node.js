24f0b6be16dd0f4d2f876065c535d153cb1ec06f
jnakano <jnakano89@gmail.com>
2014-11-02 15:12:07 -0800

    Node DELETE method

--- shopping-list/server.js
+++ shopping-list/server.js
@@ -21,6 +21,22 @@ var server = http.createServer(function(req,res){
 			});
 			res.end();
 			break; 
+		case 'DELETE':
+			var pathname = url.parse(req.url).pathname;
+			var i = parseInt(pathname.slice(1),10);
+
+			if(isNaN(i)){
+				res.statusCode = 400;
+				res.end('Item id not valid');
+			}else if(!items[i]){
+				res.statusCode = 400;
+				res.end('Item id not valid');
+			}else{
+				items.splice(i,1);
+				res.end('Item deleted successfully');
+
+			}
+			break;
 	}
 
 })
