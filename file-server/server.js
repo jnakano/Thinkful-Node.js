ff35a6d9968c95aa27bcfb427886e9fe110668e5
jnakano <jnakano89@gmail.com>
2014-11-03 17:48:14 -0800

    Add error condition

--- file-server/server.js
+++ file-server/server.js
@@ -9,16 +9,29 @@ var server = http.createServer(function(req,res){
 	var url = parse(req.url);
 	var path = join(root, url.pathname);
 
-	var stream = fs.createReadStream(path);
-	stream.pipe(res);
+	fs.stat(path, function(err,stat){
+		if(err){
+			if(err.code === 'ENOENT'){
+				res.statusCode = 404;
+				res.end('File Not Found');
+			}
+			else{
+				res.statusCode = 500;
+				res.end("Internal Server Error");
+			}
+		}
+		else{
+			var stream = fs.createReadStream(path);
+			res.setHeader('Content-Length',stat.size);
+			stream.pipe(res);
+			stream.on('error',function(err){
+				res.statusCode = 500;
+				res.end("Internal Server Error");
+			})
 
-	stream.on('data',function(chunk){
-		res.write(chunk);
-	});
-
-	stream.on('end',function(){
-		res.end(); 
-	});
+		}
+	})
+	
 })
 
 
